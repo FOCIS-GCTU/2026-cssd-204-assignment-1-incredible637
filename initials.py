0436cf383bfc85014df76595a0bac9d09a36479f
incredible637 <2425403812@live.gctu.edu.gh>
2026-05-31 19:37:40 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name:Emmanuel Nii Odoi Oddoye
+# Student ID: 2425403812
+# Email: 2425403813@live.gctu.edu.gh
+# Grader: Emmanuel
+#
+# On my honor,Emmanuel Nii Odoi Oddoye, this programming assignment is my own work
+# and I have mot provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+     # Print initials in small form
 
+     print()
+     print("...EO")
+     print()
 
+     print("...EEEEEEEEEEEE***OOOOOOOOOOOO")
+     print("...EEE         ***OO        OO")
+     print("...EEE         ***OO        OO")
+     print("...EEEEEEEEEEEE***OO        OO")
+     print("...EEE         ***OO        OO")
+     print("...EEE         ***OO        OO")
+     print("...EEE         ***OO        OO")
+     print("...EEE         ***OO        OO")
+     print("...EEE         ***OO        OO")
+     print("...EEEEEEEEEEEE***OOOOOOOOOOOO")
+     print()
 
 main()
+
